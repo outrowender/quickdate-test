6746549cbad233fb644eef9d6d730028f743cb2b
Wender <outrowender@gmail.com>
2022-09-21 18:47:11 -0300

    Initial Commit

---
+++ quickdate/quickdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  quickdateApp.swift
+//  quickdate
+//
+//  Created by Wender on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct quickdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
